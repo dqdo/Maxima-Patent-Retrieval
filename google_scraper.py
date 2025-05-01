efe2ba0fcb5a3cc06627c9d707ba19f78a9aa7f6
cmf2201 <cmfrench808@gmail.com>
2025-05-01 14:39:44 +0800

    added readme

--- google_scraper.py
+++ google_scraper.py
@@ -5,14 +5,6 @@ import json
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.edge.service import Service as EdgeService
-
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
-
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
